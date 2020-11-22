d7f2cd194d370e1556d08d4cb668401fe7f60fe0
Andreas Tampubolon <andreasjr61@gmail.com>
2020-11-22 18:47:21 +0700

    first commit

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card shadow card-product">
+    <img class="card-img-top" :src=" 'assets/images/' + product.gambar " alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">{{ product.nama}}</h5>
+      <p class="card-text">
+          Harga : {{ product.harga }}
+      </p>
+      <router-link class="btn btn-success" :to="'/foods/'+product.id"><b-icon-cart></b-icon-cart> Pesan</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardProduct",
+  props: ["product"],
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/FoodDetail.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="food-detail">
+    <Navbar />
+    <div class="container">
+
+
+      <!-- Breadcrumb -->
+      <div class="row mt-4">
+        <div class="col">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <router-link class="text-dark" to="/">Home</router-link>
+              </li>
+              <li class="breadcrumb-item">
+                <router-link class="text-dark" to="/foods">Foods</router-link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Food Detail
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+          <h2><strong>Nama Makanan</strong></h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "FoodDetail",
+  components: {
+    Navbar,
+  },
+  mounted(){
+    
+  }
+};
+</script>
+
+<style>
+</style>
